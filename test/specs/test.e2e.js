f3a03d4b4c87b23ef4d52dd918a7b89304ad4b85
Deyvis Pedrosa <deyvis.pedrosa@abstracta.com.uy>
2024-10-15 14:10:16 -0300

    enhancements

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,17 +2,26 @@ import { expect, browser, $ } from '@wdio/globals'
 
 describe('Windows handles', () => {
     it('should handle multiple windows', async () => {
-        
+
         await browser.url('https://webdriver.io/')
-        console.log(await browser.getTitle())
+
+        console.log(await browser.getTitle()) // WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO
 
         const btnGithubLnk = await $('a[href="https://github.com/webdriverio/webdriverio"][class*=navbar]')
         await btnGithubLnk.waitForDisplayed()
         await btnGithubLnk.click()
 
         const handles = await browser.getWindowHandles()
+        /**
+         * INFO webdriver: COMMAND getWindowHandles()
+         * INFO webdriver: [GET] http://localhost:57027/session/ae5abdeafe0245fa9a7eed151de0391b/window/handles
+         * INFO webdriver: RESULT [
+                'EBBFCC98D57BD36E2BB528DB3D6D2E67',
+                '2343309B34378B88439E30F2BD4AC0D3'
+            ]
+         */
         await browser.switchToWindow(handles[1])
-        console.log(await browser.getTitle())
+        console.log(await browser.getTitle()) // GitHub - webdriverio/webdriverio: Next-gen browser and mobile automation test framework for Node.js
 
         const inputGotofile = await $('[aria-label="Go to file"]')
         await inputGotofile.waitForDisplayed()
--- wdio.conf.js
+++ wdio.conf.js
@@ -87,7 +87,7 @@ export const config = {
     // baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 1000 * 60,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
